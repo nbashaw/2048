21075d4d2a1a8537535c5642abfdd2d1d0ea710d
Nathan Bashaw <nbashaw@gmail.com>
2014-03-12 22:48:18 -0500

    update interface

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -1,6 +1,6 @@
 function HTMLActuator() {
   this.tileContainer    = document.querySelector(".tile-container");
-  this.scoreContainer   = document.querySelector(".score-container");
+  this.scoreContainer   = document.querySelector(".title");
   this.bestContainer    = document.querySelector(".best-container");
   this.messageContainer = document.querySelector(".game-message");
 
